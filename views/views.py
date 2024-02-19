75e019d435fd06fcdab7b5e5ba26bc4f5de03bcd
Sebastian Morales <sebasmorales05col@gmail.com>
2024-02-19 11:51:40 -0500

    Deploy prepared 4

--- views/views.py
+++ views/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from datetime import datetime
+
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-  return render(request, "views/index.html")
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
